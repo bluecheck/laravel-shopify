663f11533ddebc6543b48aa425381adad319fe6f
Andrei Losev <losev_andrei@bk.ru>
2026-05-27 10:56:57 +0300

    SPU-1013 new app bridge fix
    This reverts commit 8edb82ad8c242f86c9d5490974401f46f7cd1534.

--- src/Http/Middleware/VerifyShopify.php
+++ src/Http/Middleware/VerifyShopify.php
@@ -294,7 +294,7 @@ class VerifyShopify
             ]);
 
             if ($filteredQuery->isNotEmpty()) {
-                $target .= '?'.http_build_query($filteredQuery->toArray());
+                $target .= '?' . http_build_query($filteredQuery->toArray());
             }
         }
 
@@ -312,7 +312,7 @@ class VerifyShopify
         return Redirect::route(
             Util::getShopifyConfig('route_names.authenticate.token'),
             [
-                'shop' => $shopDomain,
+                'shop' => $shopDomain->toNative(),
                 'host' => $host,
                 'target' => $target,
             ]
@@ -501,7 +501,7 @@ class VerifyShopify
          * @return string
          */
         $formatValue = function ($val): string {
-            return is_array($val) ? '["'.implode('", "', $val).'"]' : $val;
+            return is_array($val) ? '["' . implode('", "', $val) . '"]' : $val;
         };
 
         // Nested array
