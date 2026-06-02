d4cbe8ef708e26a25c41abd34860578179280928
Andrei Losev <losev_andrei@bk.ru>
2026-06-02 16:58:31 +0300

    SPU-1013 new app bridge fix
    This reverts commit 8edb82ad8c242f86c9d5490974401f46f7cd1534.

--- src/Http/Middleware/VerifyShopify.php
+++ src/Http/Middleware/VerifyShopify.php
@@ -409,10 +409,10 @@ class VerifyShopify
                 return $newestToken;
             }
 
-            return $request->get('token');
+            return $request->get('id_token');
         }
 
-        return $this->isApiRequest($request) ? $request->bearerToken() : $request->get('token');
+        return $this->isApiRequest($request) ? $request->bearerToken() : $request->get('id_token');
     }
 
     /**
